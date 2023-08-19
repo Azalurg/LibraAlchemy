1fdfc93fb2ccf171ed4b379033aacec43425f67a
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-08-19 17:47:12 +0200

    Get simple data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,131 @@
+use std::fs;
+
+use walkdir::WalkDir;
+
+#[derive(Debug)]
+struct Book {
+    title: String,
+    directory: String,
+    cover: String,
+}
+
+#[derive(Debug)]
+struct Album {
+    title: String,
+    books: Vec<Book>,
+    cover: String,
+    directory: String,
+}
+
+#[derive(Debug)]
+struct Author {
+    name: String,
+    albums: Vec<Album>,
+    books: Vec<Book>,
+    cover: String,
+    directory: String,
+}
+
+fn check_cover(path: String) -> String {
+    if fs::metadata(&path).is_ok() {
+        path
+    } else {
+        String::new()
+    }
+}
+
+fn contains_subfolders(folder_path: &str) -> bool {
+    match fs::read_dir(folder_path) {
+        Ok(entries) => {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    if entry.file_type().map(|ft| ft.is_dir()).unwrap_or(false) {
+                        return true;
+                    }
+                }
+            }
+            false
+        }
+        Err(_) => false, // Return false on error (directory not found, permissions, etc.)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let work_dir = "/mnt/d/Audiobooks/Test"; // Change this to your desired working directory
+    let author_level = 1; // Change this to the desired level
+    let album_level = 2; // Change this to the desired level
+    let book_level = 1; // Change this to the desired level
+
+    let mut book_lib: Vec<Author> = Vec::new();
+
+    for entry in WalkDir::new(work_dir)
+        .max_depth(1) // +1 to include the desired level
+        .min_depth(1) // Ensure we're only at the desired level
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.path().is_dir())
+    {
+        let author_name = entry.path().file_name().unwrap().to_str().unwrap().to_string();
+
+        book_lib.push(Author {
+            name: author_name,
+            albums: Vec::new(),
+            books: Vec::new(),
+            cover: check_cover(entry.path().display().to_string() + "/cover.jpg"),
+            directory: entry.path().display().to_string(),
+        });
+
+        for author in WalkDir::new(book_lib.last().unwrap().directory.clone())
+            .max_depth(1) // +1 to include the desired level
+            .min_depth(1) // Ensure we're only at the desired level
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e| e.path().is_dir())
+        {
+            if contains_subfolders(author.path().display().to_string().as_str()) {
+                book_lib.last_mut().unwrap().albums.push(Album {
+                    title: author.path().file_name().unwrap().to_str().unwrap().to_string(),
+                    books: Vec::new(),
+                    cover: check_cover(author.path().display().to_string() + "/cover.jpg"),
+                    directory: author.path().display().to_string(),
+                });
+
+                for album in WalkDir::new(book_lib.last().unwrap().albums.last().unwrap().directory.clone())
+                    .max_depth(1) // +1 to include the desired level
+                    .min_depth(1) // Ensure we're only at the desired level
+                    .into_iter()
+                    .filter_map(|e| e.ok())
+                    .filter(|e| e.path().is_dir())
+                {
+                    book_lib.last_mut().unwrap().albums.last_mut().unwrap().books.push(Book {
+                        title: album.path().file_name().unwrap().to_str().unwrap().to_string(),
+                        directory: album.path().display().to_string(),
+                        cover: check_cover(album.path().display().to_string() + "/cover.jpg"),
+                    });
+                } // +1 to include the desired level
+            } else {
+                book_lib.last_mut().unwrap().books.push(Book {
+                    title: author.path().file_name().unwrap().to_str().unwrap().to_string(),
+                    directory: author.path().display().to_string(),
+                    cover: check_cover(author.path().display().to_string() + "/cover.jpg"),
+                });
+            }
+        }
+    }
+
+    for author in book_lib.iter() {
+        println!("Author: {}", author.name);
+        for album in author.albums.iter() {
+            println!("  - Album: {}", album.title);
+            for book in album.books.iter() {
+                println!("      - Book: {}", book.title);
+            }
+        }
+        for book in author.books.iter() {
+            println!("  - Book: {}", book.title);
+        }
+    }
+
+    // Now you have the structured data in the `authors` vector.
+    // You can access authors, albums, and books using the defined structs.
 }
