ca561118425f4b12e3537920d50b34da798a68d9
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-08-28 21:03:00 +0200

    Old code refactor

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,60 @@
-// use std::env;
-
-// mod builder;
-// mod scanner;
-// mod structs;
-// mod templates;
-
-// pub const VERSION: &str = "0.3.3";
-
-// fn main() {
-//     let args: Vec<String> = env::args().skip(1).collect();
-//     let mut work_dir = String::new();
-
-//     if args.len() >= 1 {
-//         work_dir = args[0].clone();
-//         println!("Working directory: {}", args[0]);
-//     } else {
-//         work_dir = String::from("./");
-//         println!("No working directory specified");
-//     }
-
-//     if work_dir.chars().last().unwrap() != '/' {
-//         work_dir = work_dir + "/";
-//     }
-
-//     let json_path = work_dir.clone() + "data.json";
-//     let output_path = work_dir.clone() + "output.html";
-
-//     println!("--- START ---");
-
-//     scanner::scan(&work_dir.clone(), &json_path);
-//     builder::build(&json_path, &output_path);
-
-//     println!("--- END ---");
-// }
-
+use std::env;
 use rocket_dyn_templates::Template;
+use std::fs::File;
+use std::io::Read;
+use serde_json;
 
-#[macro_use] extern crate rocket;
+mod scanner;
+mod structs;
+mod library;
 
-mod hbs;
+use structs::Library;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+pub const VERSION: &str = "0.3.3";
+
+fn load_data_from_json(path: &str) -> Result<Library, Box<dyn std::error::Error>> {
+    let mut file = File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let data: Library = serde_json::from_str(&contents)?;
+
+    Ok(data)
 }
+
+#[macro_use]
+extern crate rocket;
 
 #[launch]
 fn rocket() -> _ {
+    let args: Vec<String> = env::args().skip(1).collect();
+    let mut work_dir = String::new();
+
+    if args.len() >= 1 {
+        work_dir = args[0].clone();
+        println!("Working directory: {}", args[0]);
+    } else {
+        work_dir = String::from("./");
+        println!("No working directory specified");
+    }
+
+    if work_dir.chars().last().unwrap() != '/' {
+        work_dir = work_dir + "/";
+    }
+
+    let json_path = work_dir.clone() + "data.json";
+
+    println!("--- START ---");
+
+    scanner::scan(&work_dir.clone(), &json_path);
+    let data = load_data_from_json(&json_path).unwrap();
+
+    println!("--- END ---");
+
     rocket::build()
-        .mount("/", routes![index])
-        .mount("/hbs", routes![hbs::index])
-        .attach(Template::custom(|engines| {
-            hbs::customize(&mut engines.handlebars);
-        }))
+        .manage(data)
+        .mount("/", routes![library::index])
+        .attach(Template::fairing())
 }
 
 // https://github.com/SergioBenitez/Rocket/blob/master/examples/templating/src/hbs.rs
