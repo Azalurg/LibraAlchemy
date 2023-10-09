5856b6ccedd4784e2bffb878140d26affc55561a
Azalurg <91213389+Azalurg@users.noreply.github.com>
2023-10-09 12:55:58 +0200

    Fix images load up

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::env;
+use rocket::fs::NamedFile;
+use rocket::{routes, Rocket, get, State};
 use rocket_dyn_templates::Template;
+use serde_json;
+use std::env;
 use std::fs::File;
 use std::io::Read;
-use serde_json;
+use std::path::{PathBuf, Path};
 
 mod scanner;
 mod structs;
@@ -22,27 +25,37 @@ fn load_data_from_json(path: &str) -> Result<Library, Box<dyn std::error::Error>
     Ok(data)
 }
 
-#[macro_use]
-extern crate rocket;
+#[get("/<file..>", rank = 10)]
+async fn static_files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("/").join(file)).await.ok()
+}
 
-#[launch]
-fn rocket() -> _ {
+#[rocket::main]
+async fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    let mut work_dir = String::new();
+    let mut work_dir = String::from(env::current_dir().unwrap().display().to_string());
+    let mut data_store = String::from("/etc/LibraAlchemy");
 
-    if args.len() >= 1 {
-        work_dir = args[0].clone();
-        println!("Working directory: {}", args[0]);
-    } else {
-        work_dir = String::from("./");
-        println!("No working directory specified");
+    match args.len() {
+        1 => {
+            work_dir = String::from(args[0].clone());
+        },
+        2 => {
+            work_dir = String::from(args[0].clone());
+            data_store = String::from(args[1].clone());
+        },
+        _ => {},
     }
 
     if work_dir.chars().last().unwrap() != '/' {
         work_dir = work_dir + "/";
     }
 
-    let json_path = work_dir.clone() + "data.json";
+    if data_store.chars().last().unwrap() != '/' {
+        data_store = data_store + "/";
+    }
+
+    let json_path = data_store.clone() + "data.json";
 
     println!("--- START ---");
 
@@ -51,10 +64,13 @@ fn rocket() -> _ {
 
     println!("--- END ---");
 
-    rocket::build()
+    let _ = rocket::build()
         .manage(data)
         .mount("/", routes![templates::index])
+        .mount("/", routes![static_files])
         .attach(Template::fairing())
+        .launch()
+        .await;
 }
 
 // https://github.com/SergioBenitez/Rocket/blob/master/examples/templating/src/hbs.rs
--- src/templates.rs
+++ src/templates.rs
@@ -1,15 +1,12 @@
-use crate::structs::Library;
-use rocket::State;
-use serde_json::to_value;
 use self::handlebars::{Handlebars, JsonRender};
+use crate::structs::Library;
+use rocket::{get, State};
 use rocket_dyn_templates::{context, handlebars, Template};
+use serde_json::to_value;
 
 #[get("/")]
 pub fn index(data: &State<Library>) -> Template {
     let context = to_value(&**data).expect("Failed to serialize data to JSON");
 
-    Template::render(
-        "gallery/index",
-        context
-    )
+    Template::render("gallery/index", context)
 }
